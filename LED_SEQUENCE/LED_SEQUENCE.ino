9c2fb100c115185545e5466cda552b1b2425916f
Anthony <keppi@o2.pl>
2017-08-25 11:46:15 +0200

    Add files via upload

---
+++ LED_SEQUENCE/LED_SEQUENCE.ino
@@ -0,0 +1,23 @@
+#define LED_XY 2
+#define LED_XZ 9
+int i;
+
+void setup() 
+{                
+  for (i = LED_XY; i < LED_XZ+1; i++)  
+ {
+  pinMode(i, OUTPUT);
+ }
+}
+ 
+void loop() 
+{
+  for (i = LED_XY; i < LED_XZ+1; i++) 
+ {
+   digitalWrite(i, HIGH);
+   delay(200);
+   digitalWrite(i, LOW);
+ }
+
+}
+
